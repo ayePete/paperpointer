75cf0c3b8319e27f3637963ba23b98e190843781
Vivek Srikrishnan <srikrish@gmail.com>
2017-01-09 09:31:48 -0500

    Modified to work with shorter chain lengths and on ACI cluster.

--- reproduce_SM2015.py
+++ reproduce_SM2015.py
@@ -109,16 +109,17 @@ def generate_chains(T,          # time horizon over which we care about citation
                   s,            # scooping probability
                   data,         # journal data as a pandas dataframe; columns should be Journal Name, Accept Rate, Submission Time (days), impact factor
                   n_chains,     # number of chains to generate
+                  chain_len,    # length of each chain
                   n_iter):      # number of proposals for each chain
 
     n_journals = data.shape[0]
     # sample first two journals for each sequence
-    chains = pd.DataFrame(index=np.arange(0,n_chains),columns=np.arange(0,n_journals))
+    chains = pd.DataFrame(index=np.arange(0,n_chains),columns=np.arange(0,chain_len))
     samples = list(permutations(np.arange(0,n_journals),2))
     chains.ix[:,:1] = [[sample[0],sample[1]] for sample in samples]
     # fill in rest of initial chains
     j_inds = np.arange(0,n_journals)
-    chains.ix[:,2:] = [np.random.choice(j_inds[~np.in1d(j_inds,chains.loc[chain,:1])],n_journals-2,replace=False) for chain in range(n_chains)]
+    chains.ix[:,2:] = [np.random.choice(j_inds[~np.in1d(j_inds,chains.loc[chain,:1])],chain_len-2,replace=False) for chain in range(n_chains)]
     # for each chain, propose n_iter swaps. accept with probability
     # alpha = (C_prop/C_curr)*(R_prop/R_curr)*(P_prop/P_curr),
     # so that if the proposed chain is better across all objectives, it will be accepted with probability 1
@@ -127,21 +128,37 @@ def generate_chains(T,          # time horizon over which we care about citation
     # convert data to numpy array for use with functions
     data_array = data.as_matrix()
     for i in range(n_chains):
+        prop_chains = pd.DataFrame(index=np.arange(n_chains+i*n_iter,n_chains+(i+1)*n_iter),columns=np.arange(chain_len))
+        prop_objs = pd.DataFrame(index=np.arange(n_chains+i*n_iter,n_chains+(i+1)*n_iter),columns=['Citations','Submissions','Time'])
         curr_chain = chains.ix[i,:]
         # compute objectives for current chain
         C_curr = citations(data_array[curr_chain,1],data_array[curr_chain,2],data_array[curr_chain,3],T,tR,s)
         R_curr =  submissions(data_array[curr_chain,1],data_array[curr_chain,2],T,tR,s)
         P_curr =  tot_accept_time(data_array[curr_chain,1],data_array[curr_chain,2],T,tR,s)
+        # store objectives
+        objs.ix[i,'Citations'] = C_curr
+        objs.ix[i,'Submissions'] = R_curr
+        objs.ix[i,'Time'] = P_curr
         # generate new chain proposals
         for j in range(n_iter):
-            swap_ind = np.random.choice(n_journals,2,replace=False) # sample chain indices to swap in this iteration
+            swap_out_ind = np.random.choice(chain_len,1) # sample chain indices to swap in this iteration
+            swap_in_journal = np.random.choice(np.delete(j_inds,curr_chain[swap_out_ind]),1)[0]
             prop_chain = curr_chain
-            prop_chain[swap_ind[0]],prop_chain[swap_ind[1]] = prop_chain[swap_ind[1]],prop_chain[swap_ind[0]] # swap sampled elements
+            if np.in1d(swap_in_journal,curr_chain)[0]:
+                swap_in_ind = np.argwhere(curr_chain == swap_in_journal)[0]
+                prop_chain[swap_out_ind],prop_chain[swap_in_ind] = prop_chain[swap_in_ind],prop_chain[swap_out_ind] # swap sampled elements
+            else:
+                prop_chain[swap_out_ind] = swap_in_journal
+            prop_chains.ix[j+n_chains+i*n_iter,:] = prop_chain # store proposed chain
             # compute objectives for proposed chain
             C_prop = citations(data_array[prop_chain,1],data_array[prop_chain,2],data_array[prop_chain,3],T,tR,s)
             R_prop =  submissions(data_array[prop_chain,1],data_array[prop_chain,2],T,tR,s)
             P_prop =  tot_accept_time(data_array[prop_chain,1],data_array[prop_chain,2],T,tR,s)
-            # calculate acceptance probability
+            # store objectives for proposed chains
+            prop_objs.ix[j+n_chains+i*n_iter,'Citations'] = C_prop
+            prop_objs.ix[j+n_chains+i*n_iter,'Submissions'] = R_prop
+            prop_objs.ix[j+n_chains+i*n_iter,'Time'] = P_prop
+            # calculate acceptance probability to replace "best" chain with proposed chain
             p_accept = (C_prop/C_curr)*(R_prop/R_curr)*(P_curr/P_prop)
             draw = np.random.uniform(0,1)
             # if proposal accepted, replace current chain with it
@@ -151,41 +168,41 @@ def generate_chains(T,          # time horizon over which we care about citation
                 R_curr = R_prop
                 P_curr = P_prop
         # Set chain and objectives to best results
-        chains.ix[i,:] = curr_chain
-        objs.ix[i,'Citations'] = C_curr
-        objs.ix[i,'Submissions'] = R_curr
-        objs.ix[i,'Time'] = P_curr
+        chains = pd.concat([chains,prop_chains],axis=0,ignore_index=True)
+        objs = pd.concat([objs,prop_objs],axis=0,ignore_index=True) 
         # update progress bar
         progress(i,n_chains,suffix='Complete')
     # merge chains and objectives for return    
     df = pd.concat([chains,objs],axis=1)
     return df
     
+T0 = int(sys.argv[1])
 # start timer
 start = timeit.default_timer()
 
 # read data from excel file. change path to appropriate paperpointer path for file system
-os.chdir('d:\\research\\paperpointer')  # change working directory to main paperpointer directory
+#os.chdir('d:\\research\\paperpointer')  # change working directory to main paperpointer directory
 xls = pd.ExcelFile('data/Salinas_Munch_2015_S1_Table.xlsx') # open excel data file using pandas
 data = xls.parse(xls.sheet_names[0])  # read in data as pandas dataframe
 
 n_journals = data.shape[0]
 n_chains = n_journals*(n_journals-1)    # generate a chain for each combination of first two journals
-n_iter = 1000  # number of proposals for each chain
+n_iter = 10000  # number of proposals for each chain
+chain_len = 5
 
 # set time horizon (T) in days, revision time (tR) in days, scooping probability (s)
-T = 5*365
+T = T0*365
 tR = 30
 s = 0.001
 
 # generate submission chains
-df = generate_chains(T,tR,s,data,n_chains,n_iter)
+df = generate_chains(T,tR,s,data,n_chains,chain_len,n_iter)
 # write data frame to netcdf
 df_xr = xr.Dataset.from_dataframe(df)
 # Turn variable names into strings for netCDF writing
 print('\r Saving to netCDF...')
 var_names = {}
-new_var_names = ["j%d" % number for number in np.arange(n_journals)]
+new_var_names = ["j%d" % number for number in np.arange(chain_len)]
 new_var_names.extend(['Citations','Submissions','Review Time'])
 old_var_names = [number for number in np.arange(n_journals)]
 old_var_names.extend(['Citations', 'Submissions', 'Time'])
@@ -195,7 +212,7 @@ for old, new in all_var_names:
 
 df_xr.rename(var_names,inplace=True)
 
-df_xr.to_netcdf('chains.nc',mode='w')
+df_xr.to_netcdf('chains-'+str(T0)+'.nc',mode='w')
 
 # end timer and print runtime
 stop = timeit.default_timer()
